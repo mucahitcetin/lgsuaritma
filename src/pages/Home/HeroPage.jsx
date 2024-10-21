417ae2dbc8fcdf92cd7eb59584edd9a9778083c1
mucahitcetin <mucahitcetin01@gmail.com>
2024-10-21 14:38:35 +0300

    hero updated

--- src/pages/Home/HeroPage.jsx
+++ src/pages/Home/HeroPage.jsx
@@ -3,8 +3,7 @@ import React, { useEffect, useState } from 'react';
 const images = [
     'slider1.jpg',
     'slider2.png',
-    'slider1.jpg',
-    'slider2.png',
+    'slider.jpg',
 ];
 
 const HeroPage = () => {
@@ -23,11 +22,12 @@ const HeroPage = () => {
     };
 
     return (
-        <section className="relative w-full xsm:h-60   sm:h-64 md:h-96  flex items-center justify-center overflow-hidden ">
+        <section className="relative w-full max-w-screen-xl mx-auto overflow-hidden 
+                           xsm:h-60 sm:h-64 md:h-96 lg:h-[500px] flex items-center justify-center">
             <img
                 src={images[currentIndex]}
                 alt={`Slider ${currentIndex + 1}`}
-                className="w-full h-full object-contain transition-all duration-1000"
+                className="w-full h-full object-cover transition-all duration-1000"
             />
             <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex gap-2">
                 {images.map((_, index) => (
